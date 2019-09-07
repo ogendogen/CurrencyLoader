77a18462ec376201d06903ec0755ba1c0a7f6ca6
ogendogen <kosmos500@interia.pl>
2019-09-07 23:21:57 +0200

    Listening for json files

--- CurrencyLoader/Program.cs
+++ CurrencyLoader/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace CurrencyLoader
 {
@@ -10,6 +11,20 @@ namespace CurrencyLoader
     {
         static void Main(string[] args)
         {
+            FileSystemWatcher watcher = new FileSystemWatcher();
+            watcher.Path = ".";
+            watcher.Created += File_Created;
+            watcher.EnableRaisingEvents = true;
+            Console.WriteLine("Listening for .json files started");
+            Console.ReadLine();
+        }
+
+        private static void File_Created(object sender, FileSystemEventArgs e)
+        {
+            if (e.Name.EndsWith(".json"))
+            {
+                Console.WriteLine("JSON FILE: " + e.Name);
+            }
         }
     }
 }
